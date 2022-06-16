3f14e9978f95bff8bd66d4a1c8efb6ce0f3a8e2b
myxsan <yksl3461@gmail.com>
2022-06-17 02:03:57 +0300

    Added enemy AI provoked state

--- Zombie-Runner-Game/Assets/Scripts/EnemyAI.cs
+++ Zombie-Runner-Game/Assets/Scripts/EnemyAI.cs
@@ -9,6 +9,7 @@ public class EnemyAI : MonoBehaviour
     [SerializeField] private float chaseRange = 5f;
 
     private float distanceToTarget = Mathf.Infinity;
+    private bool isProvoked = false;
 
     NavMeshAgent navMeshAgent;
 
@@ -20,12 +21,44 @@ public class EnemyAI : MonoBehaviour
 
     void Update()
     {
+        ChaseProcess();
+    }
+
+    private void ChaseProcess()
+    {
         distanceToTarget = Vector3.Distance(target.position, this.transform.position);
-        
-        if(distanceToTarget <= chaseRange)
+
+        if (isProvoked)
         {
-            navMeshAgent.SetDestination(target.position);
+            EngageTarget();
         }
+        else if (distanceToTarget <= chaseRange)
+        {
+            isProvoked = true;
+        }
+    }
+
+    private void EngageTarget()
+    {
+        if(distanceToTarget >= navMeshAgent.stoppingDistance)
+        {
+            ChaseTarget();
+        }
+        
+        if(distanceToTarget <= navMeshAgent.stoppingDistance)
+        {
+            AttackTarget();
+        }
+    }
+
+    private void ChaseTarget()
+    {
+        navMeshAgent.SetDestination(target.position);
+    }
+
+    private void AttackTarget()
+    {
+        Debug.Log("Attacked to target!");
     }
 
     private void OnDrawGizmosSelected() {
