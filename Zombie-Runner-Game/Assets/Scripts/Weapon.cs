1ad86e7d0e298cc5c539afdb2992aa2cd9a4ba1c
myxsan <yksl3461@gmail.com>
2022-06-17 13:40:21 +0300

    Added weapon damage and enemy health

--- Zombie-Runner-Game/Assets/Scripts/Weapon.cs
+++ Zombie-Runner-Game/Assets/Scripts/Weapon.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class Weapon : MonoBehaviour
 {
     [SerializeField] Camera FPCamera;
-    [SerializeField] float  range = 100f;
+    [SerializeField] float range = 100f;
+    [SerializeField] float damage = 100f;
 
     void Update()
     {
@@ -18,7 +19,19 @@ public class Weapon : MonoBehaviour
     private void Shoot()
     {
         RaycastHit hit;
-        Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit);
-        Debug.Log("I hit " + hit.transform.name);
+
+        if(Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range))
+        {
+            Debug.Log("I hit " + hit.transform.name);
+            EnemyHealth target = hit.transform.GetComponent<EnemyHealth>();
+            
+            if(target == null) {return;}
+            
+            target.TakeDamage(damage);
+        }
+        else
+        {
+            return;
+        }   
     }
 }
