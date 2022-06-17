7ee01736d4143a30fb8fe6e74ad317a91374a098
myxsan <yksl3461@gmail.com>
2022-06-17 14:20:54 +0300

    Added muzzle flash and hit vfx

--- Zombie-Runner-Game/Assets/Scripts/Weapon.cs
+++ Zombie-Runner-Game/Assets/Scripts/Weapon.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,6 +8,8 @@ public class Weapon : MonoBehaviour
     [SerializeField] Camera FPCamera;
     [SerializeField] float range = 100f;
     [SerializeField] float damage = 100f;
+    [SerializeField] ParticleSystem muzzleFlash;
+    [SerializeField] GameObject hitEffect;
 
     void Update()
     {
@@ -18,20 +21,38 @@ public class Weapon : MonoBehaviour
 
     private void Shoot()
     {
+        PlayMuzzleFlash();
+        ProcessRaycast();
+    }
+
+    private void ProcessRaycast()
+    {
         RaycastHit hit;
 
-        if(Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range))
+        if (Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range))
         {
-            Debug.Log("I hit " + hit.transform.name);
+            CreateHitImpact(hit);
             EnemyHealth target = hit.transform.GetComponent<EnemyHealth>();
-            
-            if(target == null) {return;}
-            
+
+            if (target == null) { return; }
+
             target.TakeDamage(damage);
         }
         else
         {
             return;
-        }   
+        }
+    }
+
+
+    private void PlayMuzzleFlash()
+    {
+        muzzleFlash.Play();
+    }
+    private void CreateHitImpact(RaycastHit hit)
+    {
+        GameObject impact = Instantiate(hitEffect, hit.point, Quaternion.LookRotation(hit.normal));
+
+        Destroy(impact, 0.1f);
     }
 }
